fd478c6bed1ce2c9ede5e507e0e7eb6a293fd238
geraldo <geraldo@servus.at>
2013-08-22 12:07:42 +0200

    remove sidebar

--- obst/index.php
+++ obst/index.php
@@ -42,5 +42,4 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- obst/search.php
+++ obst/search.php
@@ -34,5 +34,4 @@ get_header(); ?>
 		</div><!-- #content -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
